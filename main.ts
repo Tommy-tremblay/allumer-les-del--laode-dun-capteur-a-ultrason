63dd1f04ee251592d05b8e51e087f9579f009400
Tommy-tremblay <116100321+Tommy-tremblay@users.noreply.github.com>
2022-10-27 09:28:20 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,54 @@ basic.forever(function () {
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    if (distance < 10) {
-    	
+    if (distance < 5) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (distance < 10) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    } else if (distance < 15) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+    } else if (distance < 20) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+    } else if (distance < 25) {
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+    } else if (distance < 30) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     } else {
     	
     }
